c0cc493d55e5f13346e1ea2190c9d69f8115188e
Alexander Radchenko <alex.radchenko.by@gmail.com>
2025-11-09 21:47:38 +0300

    feat: implement the extended repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -16,9 +16,31 @@ const { NotImplementedError } = require('../lib');
  *
  */
 
-function repeater(/* str, options */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+  const repeatTimes = options.repeatTimes !== undefined ? options.repeatTimes : 1;
+  const separator = options.separator !== undefined ? String(options.separator) : '+';
+  const addition = options.addition !== undefined ? String(options.addition) : '';
+  const additionRepeatTimes = options.additionRepeatTimes !== undefined ? options.additionRepeatTimes : 1;
+  const additionSeparator = options.additionSeparator !== undefined ? String(options.additionSeparator) : '|';
+
+  let additionStr = '';
+  if (addition !== undefined) {
+    for (let i = 0; i < additionRepeatTimes; i++) {
+      additionStr += addition;
+      if (i < additionRepeatTimes - 1) {
+        additionStr += additionSeparator;
+      }
+    }
+  }
+
+  let result = '';
+  for (let i = 0; i < repeatTimes; i++) {
+    result += String(str) + additionStr;
+    if (i < repeatTimes - 1) {
+      result += separator;
+    }
+  }
+  return result;
 }
 
 module.exports = {
